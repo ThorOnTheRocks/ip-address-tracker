cdf4e1f8bcc2dc0f9df88d3d97ca7edbe86550f9
gianluca galota <gianluca.galota@gmail.com>
2021-10-04 23:51:56 +0100

    config and fetch api created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from "react";
 
 // Components
 import SearchBar from "./components/SearchBar/SearchBar";
 import Map from "./components/Map/Map";
+
+// FetchIPGeoAPI
+import fetchIpGeo from "./API";
 
 // Styles
 import './sassStyles/_global.scss';
@@ -13,14 +15,53 @@ import ResultBox from "./components/ResultBox/ResultBox";
 
 
 function App() {
+
+  const [defaultIp, setDefaultIp] = useState('');
+  const [isFetching, setIsfetching] = useState(false);
+  const [queryIp, setQueryIp] = useState(null)
+
+  useEffect(() => {
+    if (queryIp) {
+      setDefaultIp(false)
+    } else {
+      setIsfetching(true)
+      fetchIpGeo(defaultIp).then((ipData) => {
+        setIsfetching(false)
+        setDefaultIp(ipData)
+      });
+    }
+  }, [queryIp])
+
+  console.log(queryIp)
+
+  const searchIp = (query) => {
+
+    setTimeout(fetchIpGeo(query).then((ipData) => {
+      setQueryIp(ipData)
+    }), 1000)
+
+    return query
+  }
+
   return (
     <main className="App">
       <div className="container__heading">
         <h1 className="heading">IP Address Tracker</h1>
       </div>
-      <SearchBar />
-      <Map />
-      <ResultBox />
+      <SearchBar onQueryIp={searchIp} />
+      <Map
+        defaultIp={defaultIp}
+        queryIp={queryIp}
+        onQueryIp={searchIp}
+
+      />
+      <ResultBox
+        isFetching={isFetching}
+        defaultIp={defaultIp}
+        queryIp={queryIp}
+        onQueryIp={searchIp}
+
+      />
     </main>
   );
 }
