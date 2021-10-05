d90766fd69c84894df47f20756123b3ad2cb4238
gianluca galota <gianluca.galota@gmail.com>
2021-10-05 16:01:33 +0100

    leaflet map update state

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -10,7 +10,11 @@ const Map = ({ defaultIp, queryIp }) => {
     <>
       {defaultIp &&
         <>
-          <MapContainer style={{ height: '80rem', zIndex: '-10' }} center={[defaultIp.location.lat, defaultIp.location.lng]} zoom={17} scrollWheelZoom={false}>
+          <MapContainer
+            style={{ height: '80rem', zIndex: '-10' }}
+            center={[defaultIp.location.lat, defaultIp.location.lng]}
+            zoom={17}
+            scrollWheelZoom={false}>
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
